bd4f0c6f18b3fe08bf0bede60c79cc463ba79b5c
lev2099 <lev2099@gmail.com>
2024-12-26 08:13:47 +0200

    Initialize Django project with basic structure and requirements.
    Set up essential project files including `urls.py`, `wsgi.py`, and `README.md`. Add placeholder files for user app (`admin.py`, `views.py`) and include a `requirements.txt` for project dependencies.

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-2oivwokdd-hqw7v%s-6f9dz9*=!+0j#6vt@!4av!s%pr@lqy4="
-
+SECRET_KEY = "<your_secret_key>"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
